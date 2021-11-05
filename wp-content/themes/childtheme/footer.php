52023d611ac56daaccded348510bc1f11c3b1e58
Emilie Schultz <emil07v9@stud.kea.dk>
2021-11-05 12:54:33 +0100

    slet footer

---
+++ wp-content/themes/childtheme/footer.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Markup
+ */
+global $markup_theme_options;
+$copyright = wp_kses_post($markup_theme_options['markup-footer-copyright']);
+if ( is_active_sidebar('footer-1') || is_active_sidebar('footer-2') || is_active_sidebar('footer-3') || is_active_sidebar('footer-4') ) {
+	$count = 0;
+	for ( $i = 1; $i <= 4; $i++ )
+	{
+		if ( is_active_sidebar( 'footer-' . $i ) )
+		{
+			$count++;
+		}
+	}
+	
+	$footer_col= 4;
+	if( $count == 4 )
+	{
+		$footer_col= 4;
+	}
+	elseif( $count == 3)
+	{
+		$footer_col= 3;
+	}
+	elseif( $count == 2)
+	{
+		$footer_col= 2;
+	}
+	elseif( $count == 1)
+	{
+		$footer_col= 1;
+	}
+}
+
+?>
+<div class="footer-full-width">
+<?php
+if ( is_active_sidebar( 'full-width-footer' ) ){
+	dynamic_sidebar( 'full-width-footer' );
+}
+?>
+</div>
+<div class="footer-wrap">
+	<div class="container">
+		<div class="row">
+			<?php
+			for ( $i = 1; $i <= 4 ; $i++ ){
+				if ( is_active_sidebar( 'footer-' . $i ) )
+				{
+					?>
+					<div class="footer-col-<?php echo $footer_col; ?>">
+						<div class="footer-top-box wow fadeInUp">
+							<?php dynamic_sidebar( 'footer-' . $i ); ?>
+						</div>
+					</div>
+					<?php
+				}
+			}
+			?>
+		</div>
+	</div>
+	<?php do_action('markup_go_to_top_hook'); ?>
+</div>
+</div><!-- main container -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+</body>
+</html>
